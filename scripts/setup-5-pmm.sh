353801d7c34d2eb4c9fdb813b66724d0fb139b60
Corey's iMac <coreyearleon@icloud.com>
2023-01-01 10:16:06 -0500

    fix pmm script

--- scripts/setup-5-pmm.sh
+++ scripts/setup-5-pmm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "local   all $PMM_USER   scram-sha-256" >> "$PGDATA/pg_hba.conf"
+echo "local	all	$PMM_USER	all	scram-sha-256" >> "$PGDATA/pg_hba.conf"
 
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_PASSWORD" <<-EOSQL
-    CREATE USER pmm WITH SUPERUSER ENCRYPTED PASSWORD '$PMM_PASSWORD';
+    CREATE USER '$PMM_USER' WITH SUPERUSER ENCRYPTED PASSWORD '$PMM_PASSWORD';
 EOSQL
 
 exec "$@"
